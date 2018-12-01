dc75f9951d0139df37a64d1984721b352242031b
rene <renegmed@yahoo.com>
2018-12-01 12:10:04 -0500

    first commit

---
+++ ipfs/src/storehash.js
@@ -0,0 +1,43 @@
+import web3 from './web3';
+
+//Your contract address
+const address = '0x571B5EcCcF12d0EEC9b34199CB38CEED83bec897';
+
+//Your contract ABI
+
+const abi = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "x",
+				"type": "string"
+			}
+		],
+		"name": "setHash",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getHash",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
+
+export default new web3.eth.Contract(abi, address);
+
+
+
